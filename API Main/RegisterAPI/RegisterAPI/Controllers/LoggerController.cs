c55c54b0ed52adf088a23833daede173e8379553
Guilherme <guilherme.silva@ecorp.com.br>
2024-02-28 14:46:24 -0300

    Update LoggerController.cs

--- API Main/RegisterAPI/RegisterAPI/Controllers/LoggerController.cs
+++ API Main/RegisterAPI/RegisterAPI/Controllers/LoggerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RegisterAPI.Application.Interface;
 using RegisterAPI.Entity.Logger;
 using RegisterAPI.Model.Common;
@@ -16,6 +17,7 @@ namespace RegisterAPI.API.Controllers
         }
 
         [HttpGet]
+        [Authorize("XApiKey")]
         public async Task<IActionResult> GettAllLogs()
         {
             ResultResponseObject<IEnumerable<Log>> resultResponse = await _loggerApp.GetAllLogs();
